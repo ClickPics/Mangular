ea397556dd1ed71bf54c0b92501f422f3dc312e4
Matthew Li <li.matthew.m@gmail.com>
2017-11-29 21:35:14 -0800

    first week view(ugly) working

--- src/app/week-switcher/week-switcher.component.ts
+++ src/app/week-switcher/week-switcher.component.ts
@@ -6,17 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./week-switcher.component.css']
 })
 export class WeekSwitcherComponent implements OnInit {
-  date: Date;
+  monday: Date;
   constructor() {
-    this.date = new Date();
+    let date = new Date();
+    this.setMonday(date);
   }
 
+  setMonday(date: Date) {
+    let weekstart = date.getDate() - date.getDay() +1;
+    this.monday = new Date(date.setDate(weekstart));
+  }
+
+
   prevWeek() {
-    
+    let date = this.monday
+    date.setDate(date.getDate() - 7)
+    this.setMonday(date);
   }
 
   nextWeek() {
-
+    let date = this.monday
+    date.setDate(date.getDate() + 7)
+    this.setMonday(date);
   }
 
   ngOnInit() {
--- src/app/week-view/week-view.component.ts
+++ src/app/week-view/week-view.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
+import { Component, OnInit, Input } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-week-view',
@@ -7,13 +7,20 @@ import {HttpClientModule} from '@angular/common/http';
   styleUrls: ['./week-view.component.css']
 })
 export class WeekViewComponent implements OnInit {
+  private BASE_URL = "http://localhost:4751/api/EventsApi";
+  private headers = new Headers({'Content-Type': 'application/json'});
 
-  weeklyData: String
-  constructor() {
-
+  @Input() monday: Date
+  week: Object
+  constructor(private http: HttpClient) {
   }
 
   ngOnInit() {
+    this.http.get(this.BASE_URL + '?startDate=' + this.monday.toISOString()).subscribe(data => {
+      // Read the result field from the JSON response.
+      this.week = data;
+      console.log(this.week)
+    });
   }
 
 }
